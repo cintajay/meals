df481de317038fea073a7095c44633ed1c629830
Cinta <cjose@orthofx.com>
2025-06-03 15:16:52 +0530

    mark as favourite

--- lib/screens/meal_details.dart
+++ lib/screens/meal_details.dart
@@ -11,6 +11,9 @@ class MealDetailsScreen extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final favoriteMeals = ref.watch(favoriteMealsProvider);
+    final isFavorite = favoriteMeals.contains(meal);
+
     return Scaffold(
       appBar: AppBar(
         title: Text(meal.title),
@@ -26,7 +29,8 @@ class MealDetailsScreen extends ConsumerWidget {
                       wasAdded ? 'Meal added as a favorite.' : 'Meal removed.'),
                 ),
               );
-            }, icon: false? Icon(Icons.star): Icon(Icons.star_border))
+            }, icon: Icon(isFavorite ? Icons.star: Icons.star_border)
+          )
         ],
       ),
       body: SingleChildScrollView(
